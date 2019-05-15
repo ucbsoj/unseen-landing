da14d0ae954fd81cebeee112f52b2e9ed4f42e0c
Lucio Villa <lucio310@gmail.com>
2019-05-15 00:43:26 -0700

    fixed smoke mirrors submodule

--- tasks/copyAssets.js
+++ tasks/copyAssets.js
@@ -13,6 +13,13 @@ module.exports = function(grunt) {
       if (!grunt.file.exists("build/assets")) shell.mkdir("-p", "build/assets");
       shell.cp("-r", "src/assets", "build");
       shell.cp("-r", "src/new-path", "build");
+      shell.cp("-r", "src/uneven-ground", "build");
+      shell.cp("-r", "src/on-the-line", "build");
+      shell.cp("-r", "src/pennies-per-hour", "build");
+      shell.cp("-r", "src/smoke-and-mirrors", "build");
+      shell.cp("-r", "src/dirty-business", "build");
+      shell.cp("-r", "src/second-class", "build");
+      shell.cp("-r", "src/stolen", "build");
     }
   });
 
