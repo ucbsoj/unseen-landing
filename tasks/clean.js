79f386d276eba343eee1998ee65c7f518dfcbd99
Lucio Villa <lucio310@gmail.com>
2019-08-23 13:53:14 -0400

    updatind publish workflow

--- tasks/clean.js
+++ tasks/clean.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask("clean", "Removes the build folder", function() {
     shell.rm("-rf", "build");
+    shell.rm("-rf", "src/**/*node_modules");
   });
 
 };
